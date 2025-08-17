c60690fec1d523dc0d2e11fcc9933b0aaf3b2e4e
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-08-17 23:12:47 +0300

    changed the destination for the database

--- conn.php
+++ conn.php
@@ -1,12 +1,17 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "tech24_atmms";
+// $servername = "localhost";
+// $username = "root";
+// $password = "root";
+// $dbname = "tech24_atmms";
+
+$servername = "sql8.freesqldatabase.com";
+$username = "sql8795282";
+$password = "net881DZEL";
+$dbname = "sql8795282";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection faild");
+    die("Connection failed: " . $conn->connect_error);
 }
