1034bcf8c3bb7efbdac51d1ee3e9890b5328eac0
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-08-18 23:00:48 +0300

    changed to accept postgresql and used .env

--- conn.php
+++ conn.php
@@ -1,17 +1,42 @@
 <?php
 
+// local
 // $servername = "localhost";
 // $username = "root";
 // $password = "root";
 // $dbname = "tech24_atmms";
 
-$servername = "sql8.freesqldatabase.com";
-$username = "sql8795282";
-$password = "net881DZEL";
-$dbname = "sql8795282";
+//https://www.freesqldatabase.com
+// $servername = "sql8.freesqldatabase.com";
+// $username = "sql8795282";
+// $password = "net881DZEL";
+// $dbname = "sql8795282";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// if ($conn->connect_error) {
+// die("Connection failed: " . $conn->connect_error);
+// }
+
+//neon
+
+$env = parse_ini_file(__DIR__ . '/.env');
+
+$host = $env['DB_HOST'];
+$dbname = $env['DB_NAME'];
+$user = $env['DB_USERNAME'];
+$password = $env['DB_PASSWORD'];
+$port = $env['DB_PORT'];
+$sslmode = $env['DB_SSLMODE'];
+
+try {
+    $conn = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=$sslmode",
+        $user,
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+    // echo "Connected successfully!";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
